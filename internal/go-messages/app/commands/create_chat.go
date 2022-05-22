808af51208de2f315de3a5dcc3461b77a5e2ae5b
Kasper Mróz <kaspermroz1234@gmail.com>
2022-05-22 11:34:57 +0200

    Implement CreateChat app command

--- internal/go-messages/app/commands/create_chat.go
+++ internal/go-messages/app/commands/create_chat.go
@@ -3,11 +3,17 @@ package commands
 import (
 	"context"
 
+	"github.com/pkg/errors"
+
 	"github.com/kaspermroz/go-message-backend/internal/go-messages/domain"
 )
 
 type CreateChatHandler interface {
 	Handle(ctx context.Context, cmd CreateChat) error
+}
+
+type UserRepository interface {
+	UserByID(ctx context.Context, userID domain.UUID) (domain.User, error)
 }
 
 type CreateChat struct {
@@ -17,14 +23,33 @@ type CreateChat struct {
 }
 
 type createChatHandler struct {
-	repository ChatRepository
+	chatRepository ChatRepository
+	userRepository UserRepository
 }
 
-func NewCreateChatHandler(repository ChatRepository) CreateChatHandler {
-	return createChatHandler{repository: repository}
+func NewCreateChatHandler(chatRepository ChatRepository, userRepository UserRepository) CreateChatHandler {
+	return createChatHandler{
+		chatRepository: chatRepository,
+		userRepository: userRepository,
+	}
 }
 
 func (h createChatHandler) Handle(ctx context.Context, cmd CreateChat) error {
-	// TODO implement
-	return nil
+	var users []domain.User
+
+	for _, id := range cmd.UserIDs {
+		user, err := h.userRepository.UserByID(ctx, id)
+		if err != nil {
+			return errors.Wrapf(err, "could not get user with ID %s", id.String())
+		}
+
+		users = append(users, user)
+	}
+
+	chat, err := domain.NewChat(cmd.ChatID, cmd.Title, users)
+	if err != nil {
+		return errors.Wrap(err, "could not create chat")
+	}
+
+	return h.chatRepository.UpdateChat(ctx, chat)
 }
